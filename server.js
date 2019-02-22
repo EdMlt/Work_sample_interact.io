77fd950bae8c347e7f0d37ae22570ddce210ee36
EdMlt <edouard.mallecourt@gmail.com>
2019-02-23 00:29:37 +0100

    Improvement of server.js it makes the request to interact.io for the contacts and send them to contact_list.html

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var LocalStrategy = require('passport-local').Strategy;
 var express       = require('express');
 var bodyParser    = require("body-parser");
 var request       = require('request');
+var cheerio       = require('cheerio');
+var fs            = require('fs');
 
 var app = express();
 
@@ -20,31 +22,58 @@ app.get('/Login.html', function(req, res) {
     res.sendFile(__dirname + '/Login.html')
 });
 
-//Contacts page
-app.get('/contacts_list.html', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.sendFile(__dirname + '/contacts_list.html')
+//https://internal-api-staging-lb.interact.io/v2/admin/billing/details
+
+//request to interact.io
+app.post('/post.html',function(req,res){
+    /*var email=req.body.email;
+    var password=req.body.password;*/
+    email="edouard.mallecourt@gmail.com"
+    password="Doudou_69"
+    const options={
+        url    : 'https://internal-api-staging-lb.interact.io/v2/login',
+        json   : true,
+        method : 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept'      : 'application/json'
+      },
+      body   : {
+          "username": email,
+          "password": password,
+          "client"  : ""
+      }
+  }  
+
+
+  request(options,function callback_login(error, response, body) {
+    if(response.statusCode>=200||response.statusCode<400){
+        console.log(body.token.authToken);
+        authToken_user=body.token.authToken;
+        return res.redirect('/contact_list.html');
+        }
 
 });
 
-//request to interact.io
-app.post('/contacts_list.html',function(req,res){
-    console.log('cc');
-    var email=req.body.email;
-    var password=req.body.password;
-    const options={
-        url: 'https://internal-api-staging-lb.interact.io/v2/login',
-        json:true,
-        method: 'POST',
-        headers:{'Content-Type':'application/json',
-                 'Accept': 'application/json'},
-        body:{"username": email,
-              "password": password,
-              "client":""}
-             }
-
-
-    request(options,function(error, response, body) {
-    console.log(body);
-    });
 })
+app.get('/contacts', function(req, res){
+    console.log('getcontaccts')
+    const contact_request={
+            url    : 'https://vault-staging-lb.interact.io/api/contacts/list',
+            json   : true,
+            method : 'POST',
+            headers: {
+                authToken: authToken_user,
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Accept'      : 'application/json;charset=UTF-8'
+            },
+            body:{}
+            
+        }
+        request(contact_request,function callback_login(error, response, body) {
+            if(response.statusCode>=200||response.statusCode<400){
+                console.log('cc')
+        res.send(body);
+            
+        }
+    })
